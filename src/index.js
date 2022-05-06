e59ecce904edc1d5af7dfb6c53c00def0fbc861f
Katherina Kaliaha <mellinda077@mail.ru>
2022-05-06 17:47:01 +0300

    feat: realize pressing a key on a physical keyboard highlights the key on the virtual keyboard and switching keyboard layouts between English and another language

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ const enCode = ["Backquote", "Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "
   "KeyY", "KeyU", "KeyI", "KeyO", "KeyP", "BracketLeft", "BracketRight", "Backslash", "CapsLock",
   "KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", "KeyK", "KeyL", "Semicolon", "Quote",
   "Enter", "ShiftLeft", "KeyZ", "KeyX", "KeyC", "KeyV", "KeyB", "KeyN", "KeyM", "Comma", "Period",
-  "Slash", "ArrowLeft", "ShiftRight", "ControlLeft", "AltLeft", "MetaLeft", "Space", "MetaRight", "AltRight",
-  "ArrowUp", "ArrowDown", "ArrowRight"];
+  "Slash", "ArrowUp", "ShiftRight", "ControlLeft", "AltLeft", "MetaLeft", "Space", "MetaRight", "AltRight",
+  "ArrowLeft", "ArrowDown", "ArrowRight"];
 const enLittle = ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "delete",
   "tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\",
   "caps lock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "return",
   "shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "&#9650", "shift",
+  "control", "option", "command", " ", "command", "option", "&#9668", "&#9660", "&#9658"];
+
+const ruLittle = ["]", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "delete",
+  "tab", "й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ", "ё",
+  "caps lock", "ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э", "return",
+  "shift", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю", "/", "&#9650", "shift",
   "control", "option", "command", " ", "command", "option", "&#9668", "&#9660", "&#9658"];
 // let line2= [];
 // let line3 = [];
@@ -49,46 +55,60 @@ class VirtualKeyboard {
       out += `<div class="btn ${enCode[i]}">${enLittle[i]}</div>`;
     }
     this.keyboard.innerHTML = out;
+    // document.onkeydown = (event) => {
+    //   console.log(event);
+    //   document.querySelector(`.keyboard .btn .${enCode[event]}`).classList.add("active");
+    // };
+  }
+
+  listenEvent() {
+    this.textarea.onblur = () => {
+      this.textarea.focus();
+    };
+
+    document.onkeydown = (event) => {
+      const cdn = event.code;
+      const index = enCode.indexOf(cdn);
+      const ruIndex = index;
+      if (this.lang === "en") {
+        document.querySelector(`.keyboard .${event.code}`).classList.add("active");
+        this.textarea.innerHTML += `${event.key}`;
+      }
+      if (event.ctrlKey && event.shiftKey && this.lang === "en") {
+        this.lang = "ru";
+        let out = "";
+        for (let i = 0; i < ruLittle.length; i += 1) {
+          out += `<div class="btn ${enCode[i]}">${ruLittle[i]}</div>`;
+        }
+        this.keyboard.innerHTML = out;
+      }
+      if (this.lang === "ru") {
+        this.textarea.innerHTML += ruLittle[ruIndex];
+      }
+    };
+
+    document.onkeyup = (event) => {
+      document.querySelector(`.keyboard .${event.code}`).classList.remove("active");
+    };
+
+    document.addEventListener("mousedown", (event) => {
+      if (event.target.classList.contains("btn")) {
+        event.target.classList.add("active");
+        this.textarea.innerHTML += event.target.innerHTML;
+        // this.textarea.innerHTML = document.querySelector(`.keyboard .${event.code}`).innerHTML;
+      }
+    });
+
+    document.addEventListener("mouseup", (event) => {
+      if (event.target.classList.contains("btn")) {
+        event.target.classList.remove("active");
+      }
+    });
   }
 }
 
 window.onload = () => {
   const myKeyboard = new VirtualKeyboard();
   myKeyboard.createKeyboard();
+  myKeyboard.listenEvent();
 };
-
-// function createKeyboard() {
-//   const body = document.querySelector("body");
-//   // eslint-disable-next-line prefer-const
-//   let wrapper = document.createElement("div");
-//   wrapper.classList.add("wrapper");
-//   body.appendChild(wrapper);
-//     let textarea = document.createElement("textarea");
-//     textarea.classList.add("textarea");
-//     wrapper.appendChild(textarea);
-//     let keyboard = document.createElement("div");
-//     keyboard.classList.add("keyboard");
-//     wrapper.appendChild(keyboard);
-//     // console.log(body);
-//     // console.log(wrapper);
-//     let out = "";
-//     for (let i = 0; i < enLittle.length; i++) {
-//         out += `<div class="btn ${enCode[i]}">` + enLittle[i] + "</div>";
-//     }
-//     keyboard.innerHTML = out;
-// }
-// createKeyboard();
-
-// const btn = document.querySelectorAll(".btn");
-// const btnSpace = document.querySelector(".Space");
-// console.log(btnSpace);
-// function makeSpecialBtn () {
-//     for (let i = 0; i < btn.length; i++) {
-//         // console.log(btn[i]);
-//         if (btn[i].includes("Space")) {
-//             console.log(btn[i]);
-//         }
-//     }
-// }
-// makeSpecialBtn();
-// console.log(btn);
